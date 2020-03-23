2387eaa790537640d4462630480df085a2f35073
Peter Hutterer <peter.hutterer@who-t.net>
2020-03-24 08:06:46 +1000

    update to libevdev's autogen.sh
    Runs configure on its own which will reduce the CI messiness a bit
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- autogen.sh
+++ autogen.sh
@@ -1,10 +1,16 @@
-#! /bin/sh
+#!/bin/sh
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
 
-ORIGDIR=`pwd`
-cd $srcdir
+olddir=`pwd`
 
-autoreconf -v --install || exit 1
-cd $ORIGDIR || exit $?
+cd "$srcdir"
+autoreconf -fvi || exit $?
+
+cd "$olddir"
+
+git config --local --get format.subjectPrefix >/dev/null 2>&1 ||
+    git config --local format.subjectPrefix "PATCH evemu"
+
+test -n "$NOCONFIGURE" || exec "$srcdir"/configure "$@"
